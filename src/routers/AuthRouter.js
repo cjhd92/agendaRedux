88abbd51395af39f5ddd00afdfb9c7d1e67d9716
Cesar <cjhd92@gmail.com>
2022-05-07 20:35:33 -0400

    first commit redux

---
+++ src/routers/AuthRouter.js
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { RegisterScreen } from '../components/auth/RegisterScreen';
+
+export const AuthRouter = () => {
+    return (
+        <div className="auth__main">
+            <div className="auth__box-container">
+                <Routes>
+                    <Route 
+                        exact
+                        path="/auth/login"
+                        element= {<LoginScreen/>}
+                    />
+
+                    <Route 
+                        exact
+                        path="/auth/register"
+                        element= {<RegisterScreen/>}
+                    />
+
+             
+
+
+                </Routes>
+            </div>
+
+        </div>
+    )
+}
+
